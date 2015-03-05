eb9201f7cefef9d96b5d74f07f9562a6ffdc1817
cjq002 <692771080@qq.com>
2015-03-05 14:09:26 +0800

    Initial Commit

---
+++ UIAlertController代替UIALertView和UIActionSheet/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UIAlertController代替UIALertView和UIActionSheet
+//
+//  Created by 陈家庆 on 15-3-5.
+//  Copyright (c) 2015年 shikee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UIAlertController代替UIALertView和UIActionSheetTests/UIAlertController__UIALertView_UIActionSheetTests.m
@@ -0,0 +1,34 @@
+//
+//  UIAlertController__UIALertView_UIActionSheetTests.m
+//  UIAlertController代替UIALertView和UIActionSheetTests
+//
+//  Created by 陈家庆 on 15-3-5.
+//  Copyright (c) 2015年 shikee. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UIAlertController__UIALertView_UIActionSheetTests : XCTestCase
+
+@end
+
+@implementation UIAlertController__UIALertView_UIActionSheetTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
